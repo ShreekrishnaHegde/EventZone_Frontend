c3f278fcaf282b39775a968efa5addbac1f51e70
Shreekrishna Hegde <hegdeshreekrishna469@gmail.com>
2025-08-02 22:40:49 +0530

    msg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:eventzone_frontend/views/introductory_screen/onboarding_screen.dart';
+import 'package:eventzone_frontend/views/Auth_screens/login_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const OnboardingScreen(),
+      home: const LoginView(),
     );
   }
 }
